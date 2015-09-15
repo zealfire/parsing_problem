1105546f119c5ec1754c61b30678fbf96220e829
ALOK PANDEY <iamalok185@gmail.com>
2015-09-16 01:40:24 +0530

    Create diff_in_json.py

---
+++ diff_in_json.py
@@ -0,0 +1,34 @@
+#import fileinput
+#{"sad":"asdsa","ada":"asdas"}
+#{"sad":"asds","ada":"asdas"}
+import re
+import sys
+data = sys.stdin.read()
+#print data
+if(re.split('\n+',data)):
+	lines=re.split('\n+',data)
+map1={}
+map2={}
+k=0
+for line in lines:
+	#print line
+	k+=1
+	line=re.split(",",line)
+	#print line
+	for indi in line:
+		splits=re.findall(r'["].*["][:]["].*["]',indi)
+		#print splits
+		for split in splits:
+			mid=re.split(":",split)
+			key=mid[0]
+			value=mid[1]
+			if(k==1):
+				map1[key]=value
+			else:
+				map2[key]=value
+for key,value in map1.iteritems():
+	if(map1[key]<>map2[key]):
+		print (key)
+	#print key,value
+#for key,value in map2.iteritems():
+#	print key,value
