47eddc48cc19aa7dceef500186573940600bd642
ALOK PANDEY <iamalok185@gmail.com>
2015-09-19 01:01:46 +0530

    program made more efficient

--- replace.py
+++ replace.py
@@ -1,17 +1,8 @@
 import re
+#removes all comment in a program
 fp=open("program.c","r")
 content=fp.read()
-sentences=re.split("\n",content)
-final_content=[]
-for sent in sentences:
-	pattern1="^//.*"
-	pattern3=".//.*"
-	pattern2=".*//.*"
-	if(re.findall(r"//.*",sent)):
-		sent=re.sub("//.*","",sent,re.S|re.M)
-	elif(re.findall(r'/[*]{1}.*[*]{1}/',sent)):
-		sent=re.sub("/[*]{1}.*[*]{1}/","",sent)
-	final_content.append(sent)
-
-new_content="\n".join(final_content)
-print new_content	
+content=re.sub(re.compile("/\*.*\*/",re.DOTALL ) ,"" ,content)
+content=re.sub(re.compile(r"//.*") ,"" ,content)
+content=re.sub(re.compile(r"[\n](\s){1,}[\n]",re.DOTALL|re.MULTILINE) ,"\n" ,content)
+print content
