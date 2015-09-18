0640dbaf74f4fc5731999db9566cc3c3159ca344
ALOK PANDEY <iamalok185@gmail.com>
2015-09-18 16:11:34 +0530

    Generalised the program

--- overlapp.py
+++ overlapp.py
@@ -1,16 +1,31 @@
 import re
-sample_text="aaaaaannndnnnnnnfffhfhhgjjjwkkkllclc"
-#still need to generalize this
-list1=[w for w in re.finditer(r'(?=(a{2,2}))',sample_text)]
-print len(list1)#.size()#(list)
-list1=[w for w in re.finditer(r'(?=(n{4}))',sample_text)]
-print len(list1)
-list1=[w for w in re.finditer(r'(?=(a{5,}))',sample_text)]
-print len(list1)
-list1=[w for w in re.finditer(r'(?=(an{2,2}))',sample_text)]
-print len(list1)
-list1=[w for w in re.finditer(r'(?=(a{2}d{2}))',sample_text)]
-print len(list1)
-#list=re.findall(r'(?=aa)',sample_text)
-#for i in list1:
-#	print i.group(1)
+t=int(raw_input())
+sample_text1=raw_input()
+#sample_text1="aaaaaannndnnnnnnfffhfhhgjjjwkkkllclc"
+sample_text=[]
+for i in range(t):
+	string=raw_input()
+	sample_text=[]
+	for j in range(len(string)):
+		if(ord(string[j])>=97 and ord(string[j])<=122):
+			#print ord(string[j])
+			sample_text.append(string[j])
+		elif(string[j]=='+'):
+			ch=sample_text.pop()
+			for k in range(4):
+				sample_text.append(ch)
+		elif(string[j]=='.'):
+			ch=sample_text.pop()
+			#print ch
+			for k in range(2):
+				sample_text.append(ch)
+		elif(string[j]=='*'):
+			sample_text.append('{')
+			sample_text.append('5')
+			sample_text.append(',')
+			sample_text.append('}')
+	pattern=''.join(sample_text)
+	#print pattern
+	k=re.compile(r'(?=(%s))'%pattern)
+	list1=[w for w in k.finditer(sample_text1)]
+	print len(list1)
